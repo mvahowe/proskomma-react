1bad4eebda5e04cc610f0fbc60c612b6a79f3299
Mark Howe <mvahowe@gmail.com>
2020-12-01 12:37:33 +0100

    Export components

---
+++ index.js
@@ -0,0 +1,5 @@
+import PkDisplay from "./src/components/PkDisplay";
+import PkForm from "./src/components/PkForm";
+import PkQuery from "./src/components/PkQuery";
+
+export { PkDisplay, PkForm, PkQuery };
--- src/components/PkDisplay.js
+++ src/components/PkDisplay.js
@@ -1,4 +1,4 @@
-import React, {Component, useEffect} from 'react';
+import React, {Component} from 'react';
 
 class PkDisplay extends Component {
 
