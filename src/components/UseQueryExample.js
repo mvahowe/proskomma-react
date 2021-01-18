3ef404331f759e2244fb4d0dfc8b551a557c1d75
Mark Howe <mvahowe@gmail.com>
2021-01-18 12:04:00 +0100

    Add package information after merge

---
+++ src/components/UseQueryExample.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import PkForm from './PkForm';
+import PkQuery from './PkQuery';
+import PkDisplay from './PkDisplay';
+
+function UseQueryExample(props) {
+    const [queryResult, setQuery] = useQuery({data, query})
+}
